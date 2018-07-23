d9733e0782fcd8d613bd17f2e77a7e05f78e63fc
zhy <1292980901@qq.com>
2018-07-23 20:32:16 +0800

    修正

--- src/main/java/com/yidongzhineng/dxc/Config/WxConfig.java
+++ src/main/java/com/yidongzhineng/dxc/Config/WxConfig.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.yidongzhineng.dxc.Config;
 
 import com.github.wxpay.sdk.WXPay;
@@ -22,28 +22,4 @@ public class WxConfig {
         return wxPay;
     }
 }
-=======
-package com.yidongzhineng.dxc.Config;
 
-import com.github.wxpay.sdk.WXPay;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 将wxPay转化为bean，装入到容器中
- */
-@Configuration
-public class WxConfig {
-
-    @Autowired
-    private MyWxPayConfig wxPayConfig;
-
-    @Bean
-    public WXPay wxPay(){
-        WXPay wxPay=new WXPay(wxPayConfig);
-
-        return wxPay;
-    }
-}
->>>>>>> 246bf1c6ae17807ffb622c108e0cfce0c424cfed
